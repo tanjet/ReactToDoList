b645fbd24ef1f2bc51bd991d326931bc77d6e3df
Tanjet <144711080+tanjet@users.noreply.github.com>
2025-01-14 22:30:41 -0330

    uploading files

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+export default function App()
+{
+  const[newItem, setNewItem]=useState("")
+  const[todos, setTodos]=useState([])
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) =>{
+      return
+      [...currentTodos, 
+      {id: crypto.randomUUID(), 
+        title: newItem,
+         completed: false },]})
+    
+  }
+  return ( 
+    <>
+  <form onSubmit={handleSubmit} className="new-item-form">
+    <div
+      className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text " id="item"/>
+    </div>
+    <button className="btn">Add</button>
+  </form>
+  <h1 className="header">Todo List</h1>
+  <ul className="list">
+    
+    <li>
+      <label>
+        <input type="checkbox" />
+        Item1
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox" />
+        Item2
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+  </ul>
+  </>
+  )
+}
